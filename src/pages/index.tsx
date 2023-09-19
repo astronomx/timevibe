1a7f184bb0cf6b1861929593acad531eb6a59ce3
Quentin Valen <85903170+astronomx@users.noreply.github.com>
2023-09-19 07:31:01 +0200

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="flex justify-center mt-10 font-bold">If this text is centered and bold tailwind is installed</h1>
+    </>
+  )
+}
